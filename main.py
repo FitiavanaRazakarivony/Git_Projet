1c154a749b2049429e8c175cabb832add972161d
Fitiavana Razakarivony <fitiavanarazakarivony00@gmail.com>
2025-02-06 23:26:23 +0200

    initi

--- main.py
+++ main.py
@@ -1,35 +1,5 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager
-from flask_cors import CORS  # Importer Flask-CORS
+from app import create_app
 
-# Initialiser l'extension SQLAlchemy
-db = SQLAlchemy()
-jwt = JWTManager()
-
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object('config.Config')
-
-    # Configurer Flask-CORS pour autoriser les origines
-    CORS(app, resources={r"/api/*": {"origins": "*"}})
-
-    # Initialisation des extensions
-    db.init_app(app)
-    jwt.init_app(app)
-
-    # Enregistrement des blueprints
-    # Utilisation de l'importation absolue
-    from routes import register_blueprints  # Correction de l'importation
-    register_blueprints(app)
-
-    # Créer la base de données
-    with app.app_context():
-        db.create_all()
-
-    return app  # Correction ici
-
-# Création de l'application
 app = create_app()
 
 if __name__ == '__main__':
