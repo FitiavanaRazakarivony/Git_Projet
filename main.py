bef7aa40039390e517c17045cdc47c6876fc60d9
Fitiavana Razakarivony <fitiavanarazakarivony00@gmail.com>
2025-02-06 23:56:02 +0200

    init

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from app import create_app
+from waitress import serve
 
 app = create_app()
 
+
+@app.route('/')
+def hello():
+    return "Hello, World!"
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    serve(app, host='0.0.0.0', port=8000)
