eb271ba03493518af3ea73320ae7abf8100e63f4
Fitiavana Razakarivony <fitiavanarazakarivony00@gmail.com>
2025-02-07 08:24:12 +0200

    INTI

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
 # Mise à jour du système et installation de la bibliothèque libGL.so.1
 apt-get update
 apt-get install -y libgl1-mesa-glx
-
-# Lancer Gunicorn (assurez-vous que la commande Gunicorn correspond à votre configuration)
-gunicorn --bind 0.0.0.0:8000 wsgi:app
