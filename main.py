dc5ae7060363e7a62ddb47d3f674acc1658df58f
Fitiavana Razakarivony <fitiavanarazakarivony00@gmail.com>
2025-02-07 07:41:46 +0200

    init

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 from app import create_app
+
 app = create_app()
 
-
-@app.route('/')
-def hello():
-    return "Hello, World!"
-
-# La ligne suivante n'est pas nécessaire avec Gunicorn
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
